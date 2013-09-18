2b7b80a4b8a137c4bc4f38ae1724950ea631c6ba
Matt Moriarity <matt@mattmoriarity.com>
2013-09-17 19:14:14 -0600

    Some updates to test configuration.

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,8 @@
-// Karma configuration
-
 var karma = require('karma');
-
 module.exports = function(config) {
-  config.set({
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
 
-    // list of files / patterns to load in the browser
+  config.set({
+    basePath: '',
     files: [
       'bower_components/angular/angular.js',
       'bower_components/lodash/lodash.js',
@@ -19,30 +14,13 @@ module.exports = function(config) {
       'app/scripts/**/*.coffee',
       'test/spec/**/*.coffee'
     ],
-
     frameworks: ['jasmine'],
-
-    // list of files to exclude
     exclude: [],
-
-    // test results reporter to use
-    // possible values: dots || progress || growl
-    reporters: ['progress'],
-
-    // web server port
+    reporters: ['dots', 'osx'],
     port: 8080,
-
-    // cli runner port
     runnerPort: 9100,
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: karma.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
+    logLevel: config.LOG_INFO,
     autoWatch: true,
 
     // Start these browsers, currently available:
@@ -58,19 +36,11 @@ module.exports = function(config) {
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 10000,
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
     singleRun: false,
 
     preprocessors: {
       '**/*.coffee': ['coffee']
     }
-
-    //coffeePreprocessor: {
-      //transformPath: function(path) {
-        //return path.replace(
-      //}
-    //}
   });
 };
 
